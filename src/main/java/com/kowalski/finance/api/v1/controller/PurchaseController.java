4f9f4755b7eda895853e2a0606750c283991ce2f
unknown <you@example.com>
2023-07-03 06:49:38 -0300

    ajustes

--- src/main/java/com/kowalski/finance/api/v1/controller/PurchaseController.java
+++ src/main/java/com/kowalski/finance/api/v1/controller/PurchaseController.java
@@ -5,13 +5,14 @@ import com.kowalski.finance.api.v1.response.PurchaseResponse;
 import com.kowalski.finance.domain.model.Purchase;
 import com.kowalski.finance.domain.service.PurchaseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path = "/v1/purchase")
+@RequestMapping(path = "/v1/purchase", produces = MediaType.APPLICATION_JSON_VALUE)
 public class PurchaseController {
 
     private final PurchaseService purchaseService;
